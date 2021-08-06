e737a795600c2f332d7f2bd4d56a344bfdbc7435
Yihen <liuyihen@gmail.com>
2021-08-06 14:19:55 +0800

    added a minting model to the seedlist: Every time a user completes a storage, a certain amount of tokens will be issued to the operating account

--- contracts/Treasury.sol
+++ contracts/Treasury.sol
@@ -11,17 +11,37 @@ contract Treasury is Owned, ITreasury {
     using SafeMath for uint256;
 
     address public seedToken;
-    uint256 USER_DEFULT_AMOUNT = 950;
-    uint256 TREASURY_DEFAULT_AMOUNT = 50; // 5% distributed
+    unit256 MINTABLE_AMOUNT = 2100000000000000000000; //bytes: 11100011101011101011010101110011011100100100000010100000000000000000000
+    uint256 DIV_AMOUNT = 21210000000000000000000000;
     constructor(address _seed) {
         seedToken = _seed;
     }
 
     function Mint(address verifier, address receiver) public mintable override returns(bool){
-    //function Mint(address verifier, address receiver) public override returns(bool){
         if(verifier==address(0)) {
-            ISeed(seedToken).mint( receiver, USER_DEFULT_AMOUNT * (10 ** 18));
-            ISeed(seedToken).mint( address(this), TREASURY_DEFAULT_AMOUNT * (10 ** 18));
+            if(MINTABLE_AMOUNT == 0){
+                return true;
+            }
+
+            uint256 totalSuppley = IERC20(seedToken).totalSupply();
+
+            if(totalSuppley>=DIV_AMOUNT && totalSuppley % DIV_AMOUNT == 0){
+                MINTABLE_AMOUNT = MINTABLE_AMOUNT>>1;
+                if(MINTABLE_AMOUNT==0){
+                    ISeed(seedToken).mint(address(this), 210000000000000000000000);
+                    return true;
+                }
+
+                if(MINTABLE_AMOUNT<56){  // 56: 111000
+                    ISeed(seedToken).mint(address(this), 210000000000000000000000);
+                }
+
+            }
+
+            ISeed(seedToken).mint(receiver, MINTABLE_AMOUNT);
+            if(MINTABLE_AMOUNT/100>0){
+                ISeed(seedToken).mint(address(this), MINTABLE_AMOUNT/100);
+            }
             return true;
         }
         return true;
