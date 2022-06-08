2c71f875f64310711101afaea0462fa026563d94
Yihen <liuyihen@gmail.com>
2022-06-08 17:43:25 +0800

    add private vault interface for caller

---
+++ contracts/vaults/interfaces/IPrivateVault.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.12;
+
+interface IPrivateVaultHub {
+    function saveWithMinting(string memory data, string memory cryptoLabel) external;
+
+    function saveWithoutMinting(string memory data, string memory cryptoLabel) external;
+
+    function getLabelByIndex(uint64 index) external view auth returns (string memory);
+
+    function getLabelByName(string memory name) external view auth returns (string memory);
+
+    function labelName(uint64 index) external view auth returns (string memory);
+}
